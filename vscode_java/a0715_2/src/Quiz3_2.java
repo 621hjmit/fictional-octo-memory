54b2d56ffcb79f2da4a906270d0841e24a898bb8
621hjmit <superant621@gmail.com>
2024-07-15 17:13:43 +0900

    으헤

---
+++ vscode_java/a0715_2/src/Quiz3_2.java
@@ -0,0 +1,19 @@
+public class Quiz3_2 {
+    public static void main(String[] args) {
+        int[][] array = {
+                { 95, 86 },
+                { 83, 92, 96 },
+                { 78, 83, 93, 87, 88 }
+        };
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("총 합: " + sum);
+
+    }
+}
